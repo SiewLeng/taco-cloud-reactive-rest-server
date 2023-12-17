3fbe8239ff20c67aab9e9b1dc30e0ee066fe0f18
Lim Siew Leng <siewleng.lim@gmail.com>
2023-12-17 20:03:27 +0800

    First commit

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS countries (
+    id int NOT NULL,
+    name varchar(25) NOT NULL,
+    PRIMARY KEY (id)
+);
---
+++ src/test/java/tacos/TacoCloudReactiveRestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudReactiveRestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
